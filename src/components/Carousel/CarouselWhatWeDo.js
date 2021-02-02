dc46bcd96b03ec996aaeb3c9a3c702adac1c891b
Nathan Gardner <nathan@lot.to>
2021-02-02 21:59:14 +0000

    added image for what we do comp

--- src/components/Carousel/CarouselWhatWeDo.js
+++ src/components/Carousel/CarouselWhatWeDo.js
@@ -8,7 +8,10 @@ import ProductSlide from '../ProductSlide/ProductSlide'
 // Load assets
 import LeftHandle from '../../img/leftHandle.svg'
 import RightHandle from '../../img/rightHandle.svg'
-import stockCable from '../../img/stockWire.jpg'
+import wirePrep from '../../img/wire-preparation.png'
+import wiring from '../../img/wiring-harnesses.png'
+import cable from '../../img/cable-assemblies.png'
+import control from '../../img//control-panels.png'
 
 export const SlideLeft = ({ onClick }) => {
 
@@ -46,25 +49,25 @@ const CarouselWhatWeDo = () => {
                 heading="Wire Preparation"
                 text="Wire and Cable Preparation produced in seconds using automatic high speed machinery."
                 btnLink="/wire-and-cable-preparation"
-                imgSrc={stockCable}/>
+                imgSrc={wirePrep}/>
                    
                 <ProductSlide
                 heading="Cable Assembly"
                 text="Cable assemblies manufactured through the most efficient operations and highest quality standards."
                 btnLink="/cable-assembly"
-                imgSrc={stockCable}/>
+                imgSrc={cable}/>
 
                 <ProductSlide
                 heading="Wiring Harnesses"
                 text="Wiring Looms and Harnesses made on a fast turnaround and delivered with 100% electrical testing."
                 btnLink="/wiring-harness"
-                imgSrc={stockCable}/>
+                imgSrc={wiring}/>
         
                 <ProductSlide
                 heading="Control Panels"
                 text="Control Panels assembled in low and high volume by skilled and experienced panel wiring builders."
                 btnLink="/control-panel"
-                imgSrc={stockCable}/>
+                imgSrc={control}/>
             </Carousel>
         </Fragment>
     )
