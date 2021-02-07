2648c78658f5d8c56d239150319901a434d7c1e1
Nathan Gardner <nathan@lot.to>
2021-02-07 23:59:19 +0000

    fix max width on news card

--- src/components/Swiper/Swiper.js
+++ src/components/Swiper/Swiper.js
@@ -25,24 +25,24 @@ const SwipeContainer = ({ data, title }) => {
                         {title}
                     </Heading>
                 </Box>
-                <Swiper
-                spaceBetween={30}
-                slidesPerView={isLargerThan767 ? 2 : 1}
-                scrollbar={{ draggable: true }}
-                >
-                {data.length && data.map((item) => {
-                    const { frontmatter, fields } = item.node
+                    <Swiper
+                    spaceBetween={30}
+                    slidesPerView={isLargerThan767 ? 2 : 1}
+                    scrollbar={{ draggable: true }}
+                    >
+                    {data.length && data.map((item) => {
+                        const { frontmatter, fields } = item.node
 
-                    return (
-                        <SwiperSlide>
-                            <Box width="100%" maxWidth={{base: "300px", md: "100%", lg: "700px"}} height="100%">
-                                <CardArticle title={frontmatter.title} slug={fields.slug} imgFluid={frontmatter.image} date={`${frontmatter.date} - ASL Team`} />
-                            </Box>
-                        </SwiperSlide> 
-                    )
-                    })}
-                </Swiper>
-                <div className="swiper-scrollbar" data-carousel-articles="scrollbar"></div>
+                        return (
+                            <SwiperSlide>
+                                <Box width="100%" maxWidth={{base: "300px", md: "100%", lg: "580px"}} height="100%">
+                                    <CardArticle title={frontmatter.title} slug={fields.slug} imgFluid={frontmatter.image} date={`${frontmatter.date} - ASL Team`} />
+                                </Box>
+                            </SwiperSlide> 
+                        )
+                        })}
+                    </Swiper>
+                    <div className="swiper-scrollbar" data-carousel-articles="scrollbar"></div>
             </Box>
         </Box>
     )
