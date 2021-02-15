ca27a88ed18dd0e12461e5ce5a12a7660350b880
Nathan Gardner <nathan@lot.to>
2021-02-15 12:46:13 +0000

    reverse package to older version

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,7 @@ const path = require('path')
 const slugify = require('slugify')
 const { paginate } = require('gatsby-awesome-pagination')
 const { createFilePath } = require('gatsby-source-filesystem')
-// const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
 // Replacing '/' would result in empty string which is invalid
 // Remove trailing /
@@ -103,7 +103,7 @@ exports.createPages = ({ actions, graphql }) => {
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-  //fmImagesToRelative(node) // convert image paths for gatsby images
+  fmImagesToRelative(node) // convert image paths for gatsby images
 
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
