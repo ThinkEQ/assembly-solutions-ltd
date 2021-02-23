2f3e83f525009bf86c9ee22e68c3c73d28a7c201
Nathan Gardner <nathan@lot.to>
2021-02-23 23:11:52 +0000

    comment out imports

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,12 +2,12 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
+//import AboutPagePreview from './preview-templates/AboutPagePreview'
+//import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
-CMS.registerPreviewTemplate('index', IndexPagePreview)
-CMS.registerPreviewTemplate('about', AboutPagePreview)
+// CMS.registerPreviewTemplate('index', IndexPagePreview)
+// CMS.registerPreviewTemplate('about', AboutPagePreview)
 
   
