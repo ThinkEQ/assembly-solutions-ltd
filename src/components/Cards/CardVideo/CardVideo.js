c7854ea8e376aa875f00f0d232780a2a096a1960
Nathan Gardner <nathan@lot.to>
2021-02-25 21:34:04 +0000

    fix imght height

--- src/components/Cards/CardVideo/CardVideo.js
+++ src/components/Cards/CardVideo/CardVideo.js
@@ -5,14 +5,14 @@ import { FaPlayCircle } from 'react-icons/fa'
 
 const CardVideo = ({ title, imgSrc, click, imglocal }) => {
     return (
-        <Box borderRadius="4px" onClick={click} display="flex" cursor="pointer" flexDirection="column" background="#fff" height={{base: "370px", md:"420px", lg: "470px"}} width="100%" overflow="hidden">
+        <Box borderRadius="4px" onClick={click} display="flex" cursor="pointer" flexDirection="column" background="#fff" height={{base: "370px", md:"420px"}} width="100%" overflow="hidden">
             <Box height="50%" width="100%" position="relative" overflow="hidden">
                 <figure>
                     {!imglocal ?
                     <AspectRatio ratio={16 / 9}>
                         <Img src={`https://img.youtube.com/vi/${imgSrc}/hqdefault.jpg`} alt="youtube thumbnail"  height="230px"  width="105%" transform="scale(1.1)" filter="brightness(100%)"  transition=".6s ease-in-out" _hover={{width: "95%", filter:"brightness(50%)"}} />
                     </AspectRatio> :
-                    <Box height="100%"  width="105%" transform="scale(1.1)" filter="brightness(100%)"  transition=".6s ease-in-out" _hover={{width: "95%", filter:"brightness(50%)"}}>
+                    <Box height="250px"  width="105%" transform="scale(1.1)" filter="brightness(100%)"  transition=".6s ease-in-out" _hover={{width: "95%", filter:"brightness(50%)"}}>
                         <PreviewCompatibleImage imageInfo={imglocal} height="100%" width="100%" />
                     </Box>}
                 </figure>
